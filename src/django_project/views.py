472062cf6ac57631e670613cd3e36bddcefb7415
Makosiewicz, MichalX <michalx.makosiewicz@intel.com>
2019-06-25 16:55:34 +0200

    Add templates and views

---
+++ src/django_project/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home_page(request):
+    return render(request, "helloworld.html")
+
+def about_page(request):
+    return HttpResponse("<h1>About Page</h1>")
+
+def contact_page(request):
+    return HttpResponse("<h1>Contact Page</h1>")
