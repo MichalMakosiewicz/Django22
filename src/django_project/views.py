d1f840455537876f638ba634b5e2cfeb91ce3976
Makosiewicz, MichalX <michalx.makosiewicz@intel.com>
2019-06-26 16:08:19 +0200

    Added templates and bootstrap

--- src/django_project/views.py
+++ src/django_project/views.py
@@ -1,11 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
 
 def home_page(request):
-    return render(request, "helloworld.html")
+    title = "Homepage Title"
+    return render(request, "helloworld.html", {"title": title})
 
 def about_page(request):
-    return HttpResponse("<h1>About Page</h1>")
+    title = "About Title"
+    return render(request, "about.html", {"title": title})
 
 def contact_page(request):
-    return HttpResponse("<h1>Contact Page</h1>")
+    title = "Contact Title"
+    return render(request, "helloworld.html", {"title": title})
+
+def example_page(request):
+    context = {"title": "Example"}
+    template_name = "helloworld.html"
+    template_obj = get_template(template_name)
+    return HttpResponse(template_obj.render(context))
+
